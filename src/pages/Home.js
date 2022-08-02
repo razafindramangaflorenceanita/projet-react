7f1b424319c043fcf4998d564be2e24c660446c5
Your Name <razafindramangaflorenceanita@gmail.com>
2022-08-02 11:00:36 +0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Navigation/>
+            <div className='homeContent'>
+                <div className='content'>
+                    <h1>Florence Anità</h1>
+                    <h2>Développeur Back-end</h2>
+                    <div className='pdf'>
+                        <a href='./media/CV.pdf' target="_blank">Télécharger CV</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
